987b58fabd3885cd44159fd732e18a0e075ff67b
Ignat Vilesov <ignat.vilesov@gmail.com>
2017-06-16 13:43:40 +0300

    Initial commit

---
+++ src/ObjectEnumerationUtility.ts
@@ -0,0 +1,61 @@
+/*
+ *  Power BI Visualizations
+ *
+ *  Copyright (c) Microsoft Corporation
+ *  All rights reserved.
+ *  MIT License
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the ""Software""), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+module powerbi.extensibility.visual {
+    export function getValue<T>(objects: DataViewObjects, objectName: string, propertyName: string, defaultValue: T): T {
+        if (objects) {
+            let object = objects[objectName];
+            if (object) {
+                let property: T = <T>object[propertyName];
+                if (property !== undefined) {
+                    return property;
+                }
+            }
+        }
+        return defaultValue;
+    }
+
+    export function getCategoricalObjectValue<T>(category, index: number, objectName: string, propertyName: string, defaultValue: T): T {
+        let categoryObjects = category.objects;
+
+        if (categoryObjects) {
+            let categoryObject: DataViewObject = categoryObjects[index];
+
+            if (categoryObject) {
+                let object = categoryObject[objectName];
+                if (object) {
+                    let property: T = <T>object[propertyName];
+                    if (property !== undefined) {
+
+                        return property;
+                    }
+                }
+            }
+        }
+
+        return defaultValue;
+    }
+}
